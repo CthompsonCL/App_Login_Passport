067712316f7e21f02aa8bc33846b97f9224124f0
Christopher Thompson Huerta <c.thompsonhuerta@gmail.com>
2019-10-16 01:11:55 -0300

    Se agrega validación de campos en módulo de registro

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,36 @@ router.get('/register', (req,res) => res.render('Register'));
 
 // Register Handle
 router.post('/register',(req,res) => {
-    console.log(req.body)
-    res.send('hello')
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //Check required fields 
+    if(!name || !email || !password || !password2){
+        errors.push({ msg: 'Please fill in all files' });
+    }
+
+    //Check password matchs
+    if(password !== password2){
+        errors.push({ msg: 'Password do not match'});
+    }
+
+    //Check pass length 
+    if(password.length < 6){
+        errors.push({msg: 'Password should be at least 6 character'});
+    }
+
+    if(errors.length > 0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }else{
+        res.send('pass');
+    }
+
 });
 
 module.exports = router;
